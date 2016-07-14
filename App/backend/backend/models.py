371c0f3252396f4ef5b9d04b2d01ffaafbae2b36
Jameson Zaballos <jzaballos@wisc.edu>
2016-07-14 18:09:51 -0400

    Added small amount of exception handling and redefining color class

--- App/backend/backend/models.py
+++ App/backend/backend/models.py
@@ -7,18 +7,18 @@ class Piece(models.Model):
 	size = models.CharField(max_length=10)
 	articleType = models.CharField(max_length=50)
 	pattern = models.CharField(max_length=50)
-	mainColor = models.CharField(max_length=50)
+	# mainColor = models.CharField(max_length=50)
 	photo = models.ImageField(upload_to='images')
 
 	# Constructor for basic article of clothing, with exception handling
 	@classmethod
-	def __init__(self, brand, size, articleType, pattern, mainColor, photo):
+	def create(self, brand, size, articleType, pattern, photo):
 		try:
 			self.brand = brand
 			self.size = size
 			self.articleType = articleType
 			self.pattern = pattern
-			self.mainColor = mainColor
+			# self.mainColor = mainColor
 			self.photo = photo
 		except AttributeError:
 			# This is the part where we find out WTF exception handling is in Python
@@ -38,5 +38,19 @@ class colourLovers(models.Model):
 # This should probably be a subcategory of the article of clothing.
 class matchedClothing(models.Model):
 
+# Debating between creating a single color object that can be mapped to many different articles
+# or including color as an attribute to the article and not its own class.
 class color(models.Model):
-	# pieces = models.ManyToManyField(Piece)
+	# Colors will display a "Many to Many" relationship, as an article of clothing can have
+	# different colors, and a color can be in different articles of clothing.
+	# This way, searching doesn't include searching EACH ARTICLE OF CLOTHING for a match, but rather
+	# returns the foreign keys of the specific color.
+	pieces = models.ManyToManyField(Piece)
+
+	color = models.CharField(max_length=50)
+	name = models.CharField(max_length=50)
+
+	@classmethod
+	def create(hashValue, name)
+		self.color = hashValue
+		self.name = name
