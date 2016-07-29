1780c0d381eb23215d25d6ca8f633b73269d3aa5
Jameson Zaballos <jzaballos@wisc.edu>
2016-07-29 18:14:41 -0400

    Added popup for adding items

--- App/www/js/controllers.js
+++ App/www/js/controllers.js
@@ -24,12 +24,12 @@ angular.module('starter.controllers', [])
       $location.path(path);
   }
 
-  $scope.addNext = function(type) {
+  /*$scope.addNext = function(type) {
 
     var myEl = angular.element( document.querySelector( '#next' ) );
     myEl.addClass('animate fadeInDown');
     myEl.removeAttr('display: none');
-  }
+  } */
 
 
 })
@@ -37,7 +37,7 @@ angular.module('starter.controllers', [])
 .controller('UploadCtrl', function($scope, $stateParams, Chats) {
 
 })
-.controller('ChatsCtrl', function($scope, Chats, $http) {
+.controller('ChatsCtrl', function($scope, Chats, $http, $ionicPopup) {
   
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
@@ -46,8 +46,54 @@ angular.module('starter.controllers', [])
   $scope.request = function(url) {
     
   };
-  // Adds new item
-  // TODO:  NEED TO CHANGE VARIABLE NAMING
+
+  // Add function, adds a new article of clothing
+  $scope.add = function(type, brand, address) {
+  // GET request that links to PHP
+  // TODO: Get this running with Ionic Serve
+    $http.get('http://localhost/upload.php')
+       .then(function(res){
+        // If successful, for right now logs it to console, but WILL 
+        // add the object later
+        //console.log(res.data);  
+        //console.log(res.data.info.colors);
+        
+        Chats.add(res.data.cl_themes[0].id, "Hi", res.data.info.url, res.data.info.colors);
+    });
+  };
+
+  // Popup for adding Image
+  $scope.showPopup = function() {
+      $scope.data = {}
+    
+      // Custom popup
+      var myPopup = $ionicPopup.show({
+         templateUrl: '../www/templates/form.html',
+         title: 'Title',
+         subTitle: 'Subtitle',
+         scope: $scope,
+      
+         buttons: [
+            { text: 'Cancel' }, {
+               text: '<b>Save</b>',
+               type: 'button-positive',
+                  onTap: function(e) {
+            
+                     if (!$scope.data.model) {
+                        //don't allow the user to close unless he enters model...
+                           e.preventDefault();
+                     } else {
+                        return $scope.data.model;
+                     }
+                  }
+            }
+         ]
+      });
+
+      myPopup.then(function(res) {
+         console.log('Tapped!', res);
+      });    
+   };
   
 })
 
